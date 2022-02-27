f95f10acd5f3542a17847fdaa8ac694703b4ae1b
Aparna <Aparna@LAPTOP-ECJUFKE0>
2022-02-27 11:11:49 +0530

    Feb24 commit

--- src/Covariant.java
+++ src/Covariant.java
@@ -1,36 +1,36 @@
 // Covariant Return Type
 
 class CovariantOne {
-    String brand;
-    int id;
-    public CovariantOne()
-    {
-        brand="Dell";
-        id=1;
-    }
-    public CovariantOne info()
-    {
-        System.out.println("Brand= "+brand+ "\nId= "+id); 
-        return null;
-    }
+	String brand;
+	int id;
+	public CovariantOne()
+	{
+		brand="Dell";
+		id=1;
+	}
+	public CovariantOne info()
+	{
+		System.out.println("Brand= "+brand+ "\nId= "+id); 
+		return null;
+	}
 }
 class CovariantTwo extends CovariantOne{
-    String os;
-    public CovariantTwo()
-    {
-        brand="lenova";
-        id=2;
-        os="Windows 10";
-    }
-    public CovariantTwo info()
-    {
-        System.out.println("Brand= " +brand+ "\nId= " +id+ "\nOs= " +os);
-        return null;
-    }
-    public static void main(String[] args) {
-        CovariantOne obj1=new CovariantOne();
-        obj1.info();
-        CovariantTwo obj2=new CovariantTwo();
-        obj2.info();
-    }
+	String os;
+	public CovariantTwo()
+	{
+		brand="lenova";
+		id=2;
+		os="Windows 10";
+	}
+	public CovariantTwo info()
+	{
+		System.out.println("Brand= " +brand+ "\nId= " +id+ "\nOs= " +os);
+		return null;
+	}
+	public static void main(String[] args) {
+		CovariantOne obj1=new CovariantOne();
+		obj1.info();
+		CovariantTwo obj2=new CovariantTwo();
+		obj2.info();
+	}
 }
